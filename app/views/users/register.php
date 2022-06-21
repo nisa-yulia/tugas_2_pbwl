64fb190fa70917af1caf55c24ebc5021a8399eb0
nisa-yulia <105896378+nisa-yulia@users.noreply.github.com>
2022-06-21 11:32:09 +0700

    Add files via upload

---
+++ app/views/users/register.php
@@ -0,0 +1,49 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<div class="row">
+    <div class="col-md-6 mx-auto">
+        <div class="card bg-light mt-5">
+            <div class="card-header card-text">
+                <h2 class="card-text">Create Account</h2>
+            </div>
+        
+            <div class="card-body">
+                <form method="post" action="<?php echo URLROOT ;?>/users/register">
+                    <div class="form-group">
+                        <label for="name">Name<sub>*</sub></label>
+                        <input type="text" name="name" class="form-control form-control-lg <?php echo (!empty($data['name_err'])) ? 'is-invalid' : '' ;?>" value="<?php echo $data['name'] ;?>">
+                        <span class="invalid-feedback"><?php echo $data['name_err'] ;?> </span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email<sub>*</sub></label>
+                        <input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : '' ;?>" value="<?php echo $data['email'] ;?>">
+                        <span class="invalid-feedback"><?php echo $data['email_err'] ;?> </span>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="password">Password<sub>*</sub></label>
+                        <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($data['password_err'])) ? 'is-invalid' : '' ;?>" value="<?php echo $data['password'] ;?>">
+                        <span class="invalid-feedback"><?php echo $data['name_err'] ;?> </span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="confirm_password">Confirm Password<sub>*</sub></label>
+                        <input type="password" name="confirm_password" class="form-control form-control-lg <?php echo (!empty($data['confirm_password_err'])) ? 'is-invalid' : '' ;?>" value="<?php echo $data['confirm_password'] ;?>">
+                        <span class="invalid-feedback"><?php echo $data['confirm_password_err'] ;?> </span>
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col">
+                                <input type="submit" class="btn btn-info btn-block pull-left" value="Daftar">
+                            </div>
+                            <div class="col">
+                                <a href="<?php echo URLROOT ;?>/users/login" class="btn btn-info btn-block pull-right">Login </a>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require APPROOT . '/views/inc/footer.php'; ?>

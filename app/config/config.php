9dd3dc9b884d50de6bd22a3c3b59f41f761363a3
nisa-yulia <105896378+nisa-yulia@users.noreply.github.com>
2022-06-21 10:57:25 +0700

    Create config.php

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'dbpln_nisa');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/pln_nisa');
+  // Site Name
+  define('SITENAME', 'Aplikasi PLN Sederhana');
+  // App Version
+  define('APPVERSION', '1.0.0');
